19e455838849f87e0637969248cbe5f87b399d35
Gwen Ives <ives.gwen@gmail.com>
2014-10-07 13:19:18 +0200

    Added problem 35

---
+++ c/035_circular_primes.c
@@ -0,0 +1,63 @@
+/*
+ * Find the number of circular primes below N
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "math_utils.h"
+
+int main (int argc, char ** argv) {
+	if (argc != 2) {
+		fprintf (stderr, "usage: %s <N>\n", argv[0]);
+		return 1;
+	}
+
+	int N = atoi (argv[1]);
+
+	if (N <= 0) {
+		printf ("0\n");
+		return 0;
+	}
+
+	int upper_limit = pow (10, (int) log10 (N) + 1);
+
+	char * sieve = eratosthenes_sieve (upper_limit);
+
+	int count = 0;
+
+	for (int i = 2; i < N; i++) {
+		char digits[20];
+
+		sprintf (digits, "%d", i);
+
+		int all_primes = 1;
+		int rotated_num = i;
+
+		do {
+			if (!sieve[rotated_num]) {
+				all_primes = 0;
+				break;
+			}
+
+			int leftmost = digits[0];
+			int digit_index = 0;
+
+			for (digit_index = 1; digits[digit_index] != '\0'; digit_index++)
+				digits[digit_index - 1] = digits[digit_index];
+
+			digits[digit_index - 1] = leftmost;
+
+			rotated_num = atoi (digits);
+		} while (rotated_num != i);
+
+		if (all_primes)
+			count++;
+	}
+
+	free (sieve);
+
+	printf ("%d\n", count);
+
+	return 0;
+}
