295caec2d0de2c83afcf5a7e3c63f5d3f0d7d505
Gwen Ives <ives.gwen@gmail.com>
2014-10-08 08:22:52 +0200

    Added problem 40

---
+++ c/040_champernowne.c
@@ -0,0 +1,56 @@
+/*
+ * Find the product of the digits in the Champernowne's constant's fractional part falling on the first N power of 10 numbered indexes
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char ** argv) {
+	if (argc != 2) {
+		fprintf (stderr, "usage: %s <N>\n", argv[0]);
+		return 1;
+	}
+
+	int N = atoi (argv[1]);
+
+	if (N < 1) {
+		printf ("0\n");
+		return 0;
+	}
+
+	int num = 1;
+	int num_digits = 1;
+	int next_digit_at = 10;
+
+	int index = 0;
+	int next_index_at = 1;
+	int indices_found = 0;
+
+	int product = 1;
+
+	while (indices_found < N) {
+		index += num_digits;
+
+		if (index >= next_index_at) {
+			int num_containing_index = num;
+
+			for (int i = index; i > next_index_at; i--)
+				num_containing_index /= 10;
+
+			product *= num_containing_index % 10;
+			indices_found++;
+
+			next_index_at *= 10;
+		}
+
+
+		if (++num == next_digit_at) {
+			num_digits++;
+			next_digit_at *= 10;
+		}
+	}
+
+	printf  ("%d\n", product);
+
+	return 0;
+}
