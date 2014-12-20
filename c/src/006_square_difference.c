efdd129945d0207793e0f75cd2f7fb9319059a43
Gwen Ives <ives.gwen@gmail.com>
2014-12-20 11:34:58 +0100

    Calculate the sum of squares in constant time

--- c/src/006_square_difference.c
+++ c/src/006_square_difference.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include "math_utils.h"
 
+static long get_sum_of_squares (int);
+
 int main (int argc, char ** argv) {
 	if (argc != 2) {
 		fprintf (stderr, "usage: %s <N>\n", argv[0]);
@@ -17,15 +19,19 @@ int main (int argc, char ** argv) {
 	if (N < 1)
 		return 1;
 
-	long square_of_sum = arithmetic_sequence_sum (1, N + 1, 1);
-	square_of_sum *= square_of_sum;
+	long sum = arithmetic_sequence_sum (1, N + 1, 1);
+	long sum_of_squares = get_sum_of_squares (N);
 
-	long sum_of_squares = 0;
-
-	for (int i = 1; i <= N; i++)
-		sum_of_squares += i * i;
-
-	printf ("%ld\n", square_of_sum - sum_of_squares);
+	printf ("%ld\n", sum * sum - sum_of_squares);
 
 	return 0;
+}
+
+static long get_sum_of_squares (int up_to) {
+	long sum = 0;
+
+	sum = (((2 * up_to + 3) * up_to) + 1) * up_to;
+	sum /= 6;
+
+	return sum;
 }
